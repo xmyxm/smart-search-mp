b27b88808a4104a1f01c336ff4f6e0c0f1adccdc
chenxuanfeng <chenxuanfeng@meituan.com>
2024-07-22 19:35:41 +0800

    feat: up

--- miniprogram/pages/tool/tool.ts
+++ miniprogram/pages/tool/tool.ts
@@ -10,14 +10,14 @@ Component({
 			const link = event.currentTarget.dataset.value
 			wx.setClipboardData({
 				data: link,
-				success: function () {
+				success() {
 					wx.showToast({
 						title: '复制成功',
 						icon: 'success',
 						duration: 2000,
 					})
 				},
-				fail: function () {
+				fail() {
 					wx.showToast({
 						title: '复制失败',
 						icon: 'none',
