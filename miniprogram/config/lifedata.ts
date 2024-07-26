c10b9fd091bcacff7dffa82cbad69d1e0d3d29c2
chenxuanfeng <chenxuanfeng@meituan.com>
2024-07-27 07:30:14 +0800

    feat: up

--- miniprogram/config/lifedata.ts
+++ miniprogram/config/lifedata.ts
@@ -3,11 +3,6 @@ export const lifeList = [
 		name: '优秀社区',
 		list: [
 			{
-				name: '学习路线图',
-				link: 'https://roadmap.sh/roadmaps',
-				icon: 'https://p0.meituan.net/travelcube/63e3f856d81210e364c2d0a8a48b6ad04884.png',
-			},
-			{
 				name: '掘金',
 				link: 'https://juejin.im/',
 				icon: 'https://p1.meituan.net/scarlett/679994df211c029a90e8836c33b9941f504.png',
@@ -16,126 +11,6 @@ export const lifeList = [
 				name: '人人都是产品经理',
 				link: 'http://www.woshipm.com/',
 				icon: 'https://p1.meituan.net/scarlett/181941f1cba6272483f26102a29092797038.png',
-			},
-			{
-				name: '京东零售',
-				link: 'https://jelly.jd.com/',
-				icon: 'https://p1.meituan.net/scarlett/e9ecbd2d86e1b280788aa25537f0ffef462.png',
-			},
-			{
-				name: '美团技术团队',
-				link: 'https://tech.meituan.com/',
-				icon: 'https://p0.meituan.net/scarlett/c4aae7fc45c707bd335e82836736407934213.png',
-			},
-			{
-				name: '凹凸实验室',
-				link: 'https://aotu.io/',
-				icon: 'https://p1.meituan.net/scarlett/d1707f59802b64b6655911e4609833481266.png',
-			},
-			{
-				name: '阿里开发者社区',
-				link: 'https://developer.aliyun.com/group/alitech#/?_k=4b3je2',
-				icon: 'https://p1.meituan.net/scarlett/2cef921ba9602766b318d884dbadd6ea2344.jpg',
-			},
-			{
-				name: '腾讯社交用户体验设计',
-				link: 'https://isux.tencent.com/',
-				icon: 'https://p0.meituan.net/scarlett/6d27aae27bcb1a712c8ba0aa2631e68e2175.png',
-			},
-			{
-				name: 'InfoQ前端专栏',
-				link: 'https://www.infoq.cn/topic/33',
-				icon: 'https://p0.meituan.net/scarlett/77be0989a3116333d44a6487fb288bcf2190.png',
-			},
-		],
-	},
-	{
-		name: '进阶指南',
-		list: [
-			{
-				name: '前端工匠',
-				link: 'https://github.com/ljianshu/Blog',
-				icon: 'https://p0.meituan.net/scarlett/1eb0c02d7188b204f9b7beea15981e3c3095.png',
-			},
-			{
-				name: '前端乱炖',
-				link: 'https://github.com/KieSun/fucking-frontend',
-				icon: 'https://p1.meituan.net/scarlett/0fcaa7714d894f127ebd3ad3bc14ec6f1772.png',
-			},
-			{
-				name: '前端知识每日3+1',
-				link: 'https://github.com/haizlin/fe-interview',
-				icon: 'https://p0.meituan.net/scarlett/55a6c245373184983343601d79c71573544.png',
-			},
-			{
-				name: '前端博文',
-				link: 'https://github.com/fengshi123/blog',
-				icon: 'https://p0.meituan.net/scarlett/31813044616eb5d5bd78690021a5e5361210.png',
-			},
-			{
-				name: '前端游乐场',
-				link: 'http://www.godbasin.com/front-end-basic/front-end/front-end-1.html',
-				icon: 'https://p0.meituan.net/scarlett/7cf849e3e75e1cad7b6ec68b6bc606931068.png',
-			},
-			{
-				name: '每日一题',
-				link: 'https://q.shanyue.tech/fe/',
-				icon: 'https://p0.meituan.net/scarlett/85f5fa7cd45444871ee213cecbd9eaa81108.png',
-			},
-			{
-				name: '现代JavaScript教程',
-				link: 'https://zh.javascript.info/',
-				icon: 'https://p0.meituan.net/scarlett/1e6027f2e97ad73f3260dae6213996c4781.png',
-			},
-			{
-				name: 'Js 33个概念',
-				link: 'https://github.com/stephentian/33-js-concepts',
-				icon: 'https://p0.meituan.net/scarlett/ab49725a6dcafe1d7eed38d0c50763af865.png',
-			},
-			{
-				name: 'Js指南',
-				link: 'https://tsejx.github.io/javascript-guidebook/',
-				icon: 'https://p1.meituan.net/scarlett/f40931f788ba4a3c57a489d26d3b85b62573.png',
-			},
-			{
-				name: '前端学习路线',
-				link: 'https://objtube.github.io/front-end-roadmap/#/',
-				icon: 'https://p0.meituan.net/scarlett/65113f8482425227bd3f23335bc5f5856747.png',
-			},
-			{
-				name: 'leetcode',
-				link: 'https://leetcode-cn.com/',
-				icon: 'https://p0.meituan.net/scarlett/d97b16418b6e4bd6864eb1d6d8a597ea1353.png',
-			},
-			{
-				name: '豆米博客',
-				link: 'https://blog.5udou.cn/#/blog/list',
-				icon: 'https://p0.meituan.net/scarlett/24a09d0ce0e3d9892a463b4ec4183ff81201.png',
-			},
-			{
-				name: '学堂在线',
-				link: 'https://www.xuetangx.com/',
-				icon: 'https://p1.meituan.net/scarlett/62378e16f7b3bf013d72871dd9890c4c1154.png',
-			},
-			{
-				name: '印记中文',
-				link: 'https://docschina.org/',
-				icon: 'https://p0.meituan.net/scarlett/36cffff7e3405d1465a78666fc2bccdf7031.jpg',
-			},
-			{
-				name: 'AlloyTeam 腾讯文档前端团队',
-				link: 'http://www.alloyteam.com/page/0/',
-				icon: 'https://p0.meituan.net/scarlett/0bd969ed415331e33a40b7cf503bb9cb9553.png',
-			},
-			{
-				name: '前端Github',
-				link: 'https://www.kwgg2020.com/',
-				icon: 'https://p1.meituan.net/scarlett/ae6d02cc1fd2fd9cdb39efde6bf2162712733.png',
-			},
-			{
-				name: '峰华',
-				link: 'https://zxuqian.cn/',
-				icon: 'https://zxuqian.cn/img/logo.webp',
 			},
 		],
 	},
