df7beef05adcfb6b698f956cd73f73c2fc62b235
chenxuanfeng <chenxuanfeng@meituan.com>
2024-07-27 16:13:40 +0800

    feat: up

--- miniprogram/pages/webview/util/platformdata.ts
+++ miniprogram/pages/webview/util/platformdata.ts
@@ -16,14 +16,14 @@ export const platformInfoList: PlatformInfoType[] = [
 	},
 	{
 		icon: 'https://p0.meituan.net/travelcube/6d7d801c69049f0ce45ddda8703b93e07067.png',
-		path: 'index/pages/h5/h5?weburl=',
+		path: 'index/pages/h5/pt/h5?weburl=',
 		appid: 'wxde8ac0a21135c07d',
 		name: '美团',
 		select: false,
 	},
 	{
 		icon: 'https://p0.meituan.net/travelcube/64987dcc63952feb618cf0100e61dc289621.png',
-		path: 'index/pages/h5/pt/h5?weburl=',
+		path: 'pages/web-view/web-view?webviewUrl=',
 		appid: 'wx2c348cf579062e56',
 		name: '美团·外卖',
 		select: false,
