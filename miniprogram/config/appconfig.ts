3e8223554f2f63c01abbc871617942c8acbbbd32
chenxuanfeng <chenxuanfeng@meituan.com>
2024-08-01 21:56:50 +0800

    feat: up

--- miniprogram/config/appconfig.ts
+++ miniprogram/config/appconfig.ts
@@ -1,5 +1,10 @@
 export default {
-	APP_VERSION: '0.0.11',
 	APP_NAME: 'smart-search-mp',
 	APPID: 'wxddad6eb2e48f7db3',
+	APP_VERSION: '0.0.11',
+	APP_UPDATE_TIME: '2024/08/01',
+	APP_UPDATE_LOGS: [
+		{ time: '2024/07/31', msg: '更新小程序商户页面生成路径功能' },
+		{ time: '2024/08/01', msg: '更新小程序首页' },
+	],
 }
