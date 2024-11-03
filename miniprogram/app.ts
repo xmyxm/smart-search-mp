9b05d382f34e414fe9d4989e1e74a879ad4ae4c9
chenxuanfeng <chenxuanfeng@meituan.com>
2024-11-03 12:41:29 +0800

    feat: up

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -1,11 +1,17 @@
+
+import { Base64 } from 'js-base64'
+
 // app.ts
 App<IAppOption>({
 	globalData: {},
 	onLaunch() {
+		console.log('加密文本测试:', Base64.encode('加密文本测试'))
+
 		// 展示本地存储能力
 		const logs = wx.getStorageSync('logs') || []
 		logs.unshift(Date.now())
 		wx.setStorageSync('logs', logs)
+
 
 		// 登录
 		wx.login({
