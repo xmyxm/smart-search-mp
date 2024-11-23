db21c4a2fba653f8dc17ada33415984b616ffea8
chenxuanfeng <chenxuanfeng@meituan.com>
2024-11-23 14:51:47 +0800

    feat: up

--- miniprogram/pages/plugin/util/config.ts
+++ miniprogram/pages/plugin/util/config.ts
@@ -5,7 +5,7 @@ export const pluginInfoList = [
 		path: 'plugin://citySelector/index?key=UKRBZ-YVOWZ-HSVXW-T6PI5-TSAOQ-XMF6F&referer=%E5%BE%AE%E6%99%BA%E6%90%9C'
 	},
 ];
-const _ = Date.now()
+// const _ = Date.now()
 
 // if (_ > 1732345261000) {
 // 	pluginInfoList.push({
@@ -13,4 +13,9 @@ const _ = Date.now()
 // 		icon: 'https://p0.meituan.net/travelcube/734eda947b8f6745d558807d7067541a2081.png',
 // 		path: 'plugin://meishi/shop?pub_id=26&type=meituan&sid=plugin'
 // 	})
+// }
+
+// "meishi": {
+// 	"version": "1.2.3",
+// 	"provider": "wx5c787b48e6a02a51"
 // }
