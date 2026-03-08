67d5db5d6971456354006f54bbda9fedffbac2c7
chenxuanfeng <chenxuanfeng@meituan.com>
2026-03-08 10:40:04 +0800

    feat: up

--- miniprogram/pages/plugin/util/config.ts
+++ miniprogram/pages/plugin/util/config.ts
@@ -4,23 +4,14 @@ export const pluginInfoList = [
 			icon: 'https://p1.meituan.net/travelcube/92b7a28ae342537df2c52529da673be367451.png',
 			path: 'plugin://citySelector/index?key=UKRBZ-YVOWZ-HSVXW-T6PI5-TSAOQ-XMF6F&referer=%E5%BE%AE%E6%99%BA%E6%90%9C',
 		},
+		// {
+		// 	name: '物流发货插件',
+		// 	icon: 'https://img.meituan.net/dpmobile/f80b58290863ee2c938cec6950b1f3d02450.png.webp',
+		// 	path: 'plugin://logisticsPlugin/send?appid=wxddad6eb2e48f7db3',
+		// },
 		{
-			name: '物流发货插件',
-			icon: 'https://img.meituan.net/dpmobile/f80b58290863ee2c938cec6950b1f3d02450.png.webp',
-			path: 'plugin://logisticsPlugin/index?appid=wxddad6eb2e48f7db3',
+			name: '外卖美食团购',
+			icon: 'https://img.meituan.net/dpmobile/7e73bc8613970a2ece30c0b4007266c452577.png.webp',
+			path: 'plugin://meishi/shop?pub_id=26&type=meituan&sid=plugin',
 		},
 ]
-// const _ = Date.now()
-
-// if (_ > 1732345261000) {
-// 	pluginInfoList.push({
-// 		name: '外卖美食团购',
-// 		icon: 'https://p0.meituan.net/travelcube/734eda947b8f6745d558807d7067541a2081.png',
-// 		path: 'plugin://meishi/shop?pub_id=26&type=meituan&sid=plugin'
-// 	})
-// }
-
-// "meishi": {
-// 	"version": "1.2.3",
-// 	"provider": "wx5c787b48e6a02a51"
-// }
