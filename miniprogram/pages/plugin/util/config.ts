7e153e11e43cf214a6197a8a26b599cbaff8be4a
chenxuanfeng <chenxuanfeng@meituan.com>
2026-03-08 10:31:06 +0800

    feat: up

--- miniprogram/pages/plugin/util/config.ts
+++ miniprogram/pages/plugin/util/config.ts
@@ -1,9 +1,14 @@
 export const pluginInfoList = [
-	{
-		name: '腾讯位置服务城市选择器',
-		icon: 'https://p1.meituan.net/travelcube/92b7a28ae342537df2c52529da673be367451.png',
-		path: 'plugin://citySelector/index?key=UKRBZ-YVOWZ-HSVXW-T6PI5-TSAOQ-XMF6F&referer=%E5%BE%AE%E6%99%BA%E6%90%9C',
-	},
+		{
+			name: '腾讯位置服务城市选择器',
+			icon: 'https://p1.meituan.net/travelcube/92b7a28ae342537df2c52529da673be367451.png',
+			path: 'plugin://citySelector/index?key=UKRBZ-YVOWZ-HSVXW-T6PI5-TSAOQ-XMF6F&referer=%E5%BE%AE%E6%99%BA%E6%90%9C',
+		},
+		{
+			name: '物流发货插件',
+			icon: 'https://img.meituan.net/dpmobile/f80b58290863ee2c938cec6950b1f3d02450.png.webp',
+			path: 'plugin://logisticsPlugin/index?appid=wxddad6eb2e48f7db3',
+		},
 ]
 // const _ = Date.now()
 
